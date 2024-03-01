32d21c62a65a0cfa41d7342f52f72be940f89192
Anindyadeep <anindyadeep@pop-os.localdomain>
2024-03-01 13:05:59 +0530

    deleted completion and postprocess files

--- light_lm_eval/postprocess.py
+++
@@ -1,7 +0,0 @@
-from typing import Optional
-from light_lm_eval.config import EvaluatorConfig
-
-
-class PostProcesser:
-    def __init__(self, config: Optional[EvaluatorConfig] = EvaluatorConfig()) -> None:
-        pass
