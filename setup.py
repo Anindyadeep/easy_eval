a40d2a48ecce79f9b884263737184a40df67dd50
Anindyadeep <anindyadeep@pop-os.localdomain>
2024-03-03 20:06:22 +0530

    Added latest version with bug fixes

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='easy_eval',
-    version='0.1.0',
+    name='easy_evaluator',
+    version='0.1.1',
     description='A library for easy evaluation of language models',
     author='Anindyadeep',
     author_email='proanindyadeep@gmail.com',
-    url='https://github.com/your_username/easy_eval',
+    url='https://github.com/Anindyadeep/easy_eval',
     packages=find_packages(),
     install_requires=[
+        # List your dependencies here from requirements.txt
         "fastapi==0.108.0",
-        "lm-eval @ git+https://github.com/EleutherAI/lm-evaluation-harness.git"
+        "lm-eval==0.4.1"
     ], 
+    include_package_data=True,
 )
