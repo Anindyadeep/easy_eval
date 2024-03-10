6f28ac244dbf17fb7989fc7c9dce615187557cf0
Anindyadeep <anindyadeep@pop-os.localdomain>
2024-03-11 01:27:10 +0530

    Added evaluate_from_model_response method to evaluate from model response

--- easy_eval/harness/evaluator.py
+++ easy_eval/harness/evaluator.py
@@ -41,30 +41,24 @@ class HarnessEvaluator:
                 f"but found {type(model)}"
             )
     
-    def evaluate_from_model_response(self):
-        raise NotImplementedError
-    
-    def evaluate(
+    def evaluate_from_model_response(
         self, 
-        tasks: Union[List[str], List[HarnessTask]], 
+        tasks: Union[List[str], List[HarnessTask]],
+        metadata: dict, 
         gen_kwargs: Optional[Union[str, dict]] = None,
         bootstrap_iters: Optional[int] = 100000
-    ) -> Tuple[dict, list]:
-        """Function to evaluate model on the specified tasks using lm-eval-harness backend
+    ) -> dict:
+        """Evaluate results from model responses 
 
         Args:
             tasks (Union[List[str], List[HarnessTask]]): A List of tasks that needs to evaluated 
+            metadata: (dict): Metadata consists for the task information 
             gen_kwargs (Optional[Union[str, dict]], optional): generation kwargs for the model. Defaults to None.
             bootstrap_iters (Optional[int], optional): Defaults to 100000.
 
-
         Returns:
-            Tuple[dict, list]: Returns the result and the raw responses from the model
+            dict: Results for each of the tasks 
         """
-        random.seed(self.config.random_seed)
-        np.random.seed(self.config.numpy_random_seed)
-        torch.manual_seed(self.config.torch_random_seed) 
-        
         assert tasks is None, ValueError("tasks can not be None.")
         
         if gen_kwargs is not None:
@@ -75,22 +69,16 @@ class HarnessEvaluator:
             if gen_kwargs == "":
                 gen_kwargs = None
         
-        self.task_dict = HarnessTaskManager.get_task_dict(tasks=tasks, num_fewshot=self.config.num_fewshot, gen_kwargs=gen_kwargs)
+        random.seed(self.config.random_seed)
+        np.random.seed(self.config.numpy_random_seed)
+        torch.manual_seed(self.config.torch_random_seed)
         
-        if isinstance(self.lm, HarnessModels):
-            raw_responses, task_dict, metadata = self.lm.generate(
-                tasks=tasks, return_task_metadata=True
-            )
-        else:
-            raw_responses, task_dict, metadata = _build_tasks_and_generate(
-                tasks=tasks, lm=self.lm, limit=self.config.limit, write_out=self.config.write_out
-            ) 
+        task_dict = HarnessTaskManager.get_task_dict(tasks=tasks, num_fewshot=self.config.num_fewshot, gen_kwargs=gen_kwargs)
         
         results = harness_postprocessor(
             lm=self.lm, task_dict=task_dict, metadata=metadata, config=self.config, bootstrap_iters=bootstrap_iters
         )
-                
-
+        
         if self.lm.rank == 0:
             model_name = self.lm.config._name_or_path
 
@@ -108,4 +96,37 @@ class HarnessEvaluator:
                 "gen_kwargs": gen_kwargs,
             }
             results["git_hash"] = get_git_commit_hash()
-            return results, raw_responses
+            return results
+        
+    def evaluate(
+        self, 
+        tasks: Union[List[str], List[HarnessTask]], 
+        gen_kwargs: Optional[Union[str, dict]] = None,
+        bootstrap_iters: Optional[int] = 100000
+    ) -> Tuple[dict, list]:
+        """Function to evaluate model on the specified tasks using lm-eval-harness backend
+
+        Args:
+            tasks (Union[List[str], List[HarnessTask]]): A List of tasks that needs to evaluated 
+            gen_kwargs (Optional[Union[str, dict]], optional): generation kwargs for the model. Defaults to None.
+            bootstrap_iters (Optional[int], optional): Defaults to 100000.
+
+
+        Returns:
+            Tuple[dict, list]: Returns the result and the raw responses from the model
+        """
+        
+        if isinstance(self.lm, HarnessModels):
+            raw_responses, _, metadata = self.lm.generate(
+                tasks=tasks, return_task_metadata=True
+            )
+        else:
+            raw_responses, _, metadata = _build_tasks_and_generate(
+                tasks=tasks, lm=self.lm, limit=self.config.limit, write_out=self.config.write_out
+            ) 
+        
+        results = self.evaluate_from_model_response(
+            tasks=tasks,
+            metadata=metadata, gen_kwargs=gen_kwargs, bootstrap_iters=bootstrap_iters
+        )
+        return results, raw_responses
