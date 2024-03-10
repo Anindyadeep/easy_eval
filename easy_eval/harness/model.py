01a5391f576153a19530c93c2eb67e46882f8985
Anindyadeep <anindyadeep@pop-os.localdomain>
2024-03-10 22:35:09 +0530

    Added a class for HarnessModel

--- easy_eval/harness/model.py
+++ easy_eval/harness/model.py
@@ -7,11 +7,13 @@ import importlib
 from pathlib import Path
 from typing import Optional, List, Union
 
+import lm_eval
 from lm_eval import evaluator, utils
 from lm_eval.utils import make_table
 from easy_eval.config import EvaluatorConfig
-from easy_eval.harness.tasks import HarnessTask
+from easy_eval.harness.tasks import HarnessTask, HarnessTaskManager
 
+from lm_eval.utils import  eval_logger
 
 # TODO: Make more indepedent component which can evaluate with LLM's output only without requiring LLMs. (do we even require this?)
 
@@ -24,6 +26,71 @@ def _handle_non_serializable(o):
     else:
         return str(o)
 
+
+class HarnessModels:
+    def __init__(
+        self,
+        model_name_or_path: str,
+        model_backend: str,
+        verbosity: Optional[str] = "INFO",
+        config: Optional[EvaluatorConfig] = EvaluatorConfig(), 
+        **kwargs,
+    ) -> None:
+        """Harness Models are the LLMs running on various inference engines but strictly compatible with lm-eval-harness's ecosystem"""
+        _model_classfile_mapper = {
+            "huggingface": ("huggingface", "HFLM"),
+            "vllm": ("vllm_causallms", "VLLM"),
+            "anthropic": ("anthropic_llms", "AnthropicLM"),
+            "gguf": ("gguf", "GGUFLM"),
+            "ggml": ("gguf", "GGUFLM"),
+            "mamba": ("mamba_lm", "MambaLMWrapper"),
+            "openvino": ("optimum_lm", "OptimumLM"),
+            "openai": ("openai_completions", "OpenaiCompletionsLM"),
+        }
+
+        _available_backends = list(_model_classfile_mapper.keys())
+
+        assert model_backend in _available_backends, ValueError(
+            f"model_backend: {model_backend} does not exist. Available backends: {_available_backends}"
+        )
+        _llm_module = getattr(
+            importlib.import_module(
+                f"lm_eval.models.{_model_classfile_mapper[model_backend][0]}"
+            ),
+            _model_classfile_mapper[model_backend][1],
+        )
+        
+        self.config = config
+        model = _llm_module(model_name_or_path, **kwargs)
+        
+        if self.config.use_cache is not None:
+            self.lm = lm_eval.api.model.CachingLM(
+                model, self.config.use_cache + "_rank" + str(model.rank) + ".db"
+            )    
+            eval_logger.info(
+                f"Using cache at {self.use_cache + '_rank' + str(model.rank) + '.db'}"
+            )
+        else:
+            self.lm = model 
+        
+
+        # other stuffs
+        self.verbosity = verbosity
+        self.eval_logger = utils.eval_logger
+        self.eval_logger.setLevel(getattr(logging, f"{verbosity}"))
+        self.eval_logger.info(f"Verbosity set to {verbosity}")
+        os.environ["TOKENIZERS_PARALLELISM"] = "false"
+    
+    def generate(self, tasks: Union[List[str], HarnessTask]) -> List[str]:
+        task_dict = HarnessTaskManager.get_task_dict(tasks)
+        task_dict, metadata = HarnessTaskManager.build_task_requests(
+            task_dict=task_dict, lm=self.lm, limit=self.config.limit, write_out=self.config.write_out
+        )
+        requests = metadata["requests"]
+        for request_type, reqs in requests.items():
+            responses = getattr(self.lm, request_type)(reqs)
+        
+        return responses 
 
 class HarnessEvaluator:
     def __init__(
