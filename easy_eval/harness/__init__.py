49edb0387e4f8212f5824dfcf7baf4bd11ef20cd
Anindyadeep <anindyadeep@pop-os.localdomain>
2024-03-11 01:41:09 +0530

    Added specific classes and methods

--- easy_eval/harness/__init__.py
+++ easy_eval/harness/__init__.py
@@ -1,5 +1,12 @@
 from easy_eval.harness.utils import harness_postprocessor
+from easy_eval.harness.model import HarnessModels
+from easy_eval.harness.tasks import HarnessTask, HarnessTaskManager
+from easy_eval.harness.evaluator import HarnessEvaluator
 
 __all__ = [
-    'harness_postprocessor'
+    "harness_postprocessor",
+    "HarnessModels",
+    "HarnessTask",
+    "HarnessTaskManager",
+    "HarnessEvaluator",
 ]
